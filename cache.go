9fdf41b41db570e442e9c965437d8c92a15dd90b
Giulio Iotti <dullgiulio@gmail.com>
2017-11-22 10:18:21 +0000

    cache: trust stat and don't run GC when nothing to do

--- cache.go
+++ cache.go
@@ -184,9 +184,11 @@ func (c *cache) gc(d time.Duration) {
 	for {
 		time.Sleep(d)
 		c.events <- func() error {
-			c.debug("running garbage collector cycle, memory is %d", c.stat.Mem)
-			c.entries.gc(time.Now(), c.stat)
-			c.debug("garbage collection done, memory is %d", c.stat.Mem)
+			if c.stat.Mem > 0 {
+				c.debug("running garbage collector cycle, memory is %d", c.stat.Mem)
+				c.entries.gc(time.Now(), c.stat)
+				c.debug("garbage collection done, memory is %d", c.stat.Mem)
+			}
 			done <- struct{}{}
 			return nil
 		}
